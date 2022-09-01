94fe2d031fbdb9e8ca6a87e973ebe4b5dccef1ba
jzinn <jzinn>
2022-09-01 11:01:10 -0700

    each

--- modules/each/src/index.js
+++ modules/each/src/index.js
@@ -1,12 +1,23 @@
 export default function each(iterable, i = 0) {
-	const iterator = iterable[Symbol.iterator]();
-	return {
+	const original = iterable[Symbol.iterator]();
+	return augment({
 		next(...args) {
-			const { done, value } = iterator.next(...args);
+			const { done, value } = original.next(...args);
 			if (done) return { done, value };
 			return { value: [value, i++, iterable] };
 		},
-		return: iterator.return.bind(iterator),
-		throw: iterator.throw.bind(iterator),
-	};
+	}, original);
+}
+
+// Write tests to prove that checking is necessary.
+function augment(iterator, original) {
+	bind(iterator, original, 'return');
+	bind(iterator, original, 'throw');
+	return iterator;
+}
+
+function bind(iterator, original, property) {
+	const fn = original[property];
+	if (typeof fn === 'function')
+		iterator[property] = fn.bind(original);
 }
