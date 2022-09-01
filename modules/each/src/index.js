715ec7f7ee2518aa6834e0c31c060f1cc3c8f098
jzinn <jzinn>
2022-09-01 10:32:16 -0700

    each

--- modules/each/src/index.js
+++ modules/each/src/index.js
@@ -1,10 +1,12 @@
 export default function each(iterable, i = 0) {
 	const iterator = iterable[Symbol.iterator]();
 	return {
-		next() {
-			const { done, value } = iterator.next();
+		next(...args) {
+			const { done, value } = iterator.next(...args);
 			if (done) return { done, value };
 			return { value: [value, i++, iterable] };
 		},
+		return: iterator.return.bind(iterator),
+		throw: iterator.throw.bind(iterator),
 	};
 }
