844ed6ce2de1cce9623c7061405a41f742e624f7
jzinn <jzinn>
2022-09-02 13:40:12 -0700

    throw

--- modules/thunk/src/index.js
+++ modules/thunk/src/index.js
@@ -1,5 +1,6 @@
 export default function thunk(fn, ...args) {
-	if (typeof fn !== 'function') throw new TypeError(`${fn} is not a function`);
+	if (typeof fn !== 'function')
+		throw new TypeError('first argument must be a function');
 	return function THUNK() {
 		return fn(...args);
 	};
