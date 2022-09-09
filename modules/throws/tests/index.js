c26176885d0ba60d4b1d33a9fb6311640387dd4d
jzinn <jzinn>
2022-09-09 12:45:42 -0700

    Fix copypasta

--- modules/throws/tests/index.js
+++ modules/throws/tests/index.js
@@ -3,7 +3,7 @@ import throws from '#throws';
 import { test } from 'uvu';
 import * as assert from 'uvu/assert';
 
-test('returns', function () {
+test('throws', function () {
 	assert.throws(throws('1'), '1');
 	assert.throws(throws('2'), '2');
 	assert.throws(throws('3'), '3');
