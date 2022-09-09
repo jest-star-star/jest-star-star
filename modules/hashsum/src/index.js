903a5ad8c22eaff6d36f0b1fcdbafc05012c3a0a
jzinn <jzinn>
2022-09-09 14:21:15 -0700

    hashsum

--- modules/hashsum/src/index.js
+++ modules/hashsum/src/index.js
@@ -1,9 +1,13 @@
 import { createHash } from 'node:crypto';
 
 export default function hashsum(items = []) {
-	return items.reduce(update, createHash('sha256')).digest('hex');
+	const hash = createHash('sha256');
+	items.reduce(update, hash);
+	return hash.digest('hex');
 }
 
 function update(hash, item) {
-	return hash.update(`${item}`).update('\0', 'utf8');
+	hash.update(`${item}`);
+	hash.update('\0', 'utf8');
+	return hash;
 }
