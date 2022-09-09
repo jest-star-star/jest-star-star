88b33db12831e81ac6170335d81fbf0b95e5715a
jzinn <jzinn>
2022-09-09 16:46:50 -0700

    jest test getCacheKey

--- modules/jest-transformer-esbuild/tests/getCacheKey.js
+++ modules/jest-transformer-esbuild/tests/getCacheKey.js
@@ -10,7 +10,68 @@ test('when given no arguments', () => {
 	assert.match(cacheKey, /^[A-Za-z0-9]+$/u);
 });
 
-// Test same nodeEnv, etc. values lead to same cacheKey values.
-// Test different nodeEnv, etc. values lead to different cacheKey values.
+function testInput(description, invoke) {
+	test(`same input same output for input ${description}`, function () {
+		assert.is(invoke(true), invoke(true));
+		assert.is(invoke(false), invoke(false));
+	});
+	test(`different input different output for input ${description}`, function () {
+		assert.is.not(invoke(true), invoke(false));
+	});
+}
+testInput('transformerId', function invoke(value) {
+	const transformerFactory = createTransformerFactory({ transformerId: value });
+	const transformer = transformerFactory.createTransformer();
+	const cacheKey = transformer.getCacheKey();
+	return cacheKey;
+});
+testInput('transformerConfigString', function invoke(value) {
+	const transformerFactory = createTransformerFactory();
+	const transformer = transformerFactory.createTransformer({
+		foo: value,
+	});
+	const cacheKey = transformer.getCacheKey();
+	return cacheKey;
+});
+testInput('sourceText', function invoke(value) {
+	const transformerFactory = createTransformerFactory();
+	const transformer = transformerFactory.createTransformer();
+	const cacheKey = transformer.getCacheKey(value);
+	return cacheKey;
+});
+testInput('sourcePath', function invoke(value) {
+	const transformerFactory = createTransformerFactory();
+	const transformer = transformerFactory.createTransformer();
+	const cacheKey = transformer.getCacheKey(undefined, value);
+	return cacheKey;
+});
+testInput('configString', function invoke(value) {
+	const transformerFactory = createTransformerFactory();
+	const transformer = transformerFactory.createTransformer();
+	const cacheKey = transformer.getCacheKey(undefined, undefined, {
+		configString: value,
+	});
+	return cacheKey;
+});
+testInput('instrumentString', function invoke(value) {
+	const transformerFactory = createTransformerFactory();
+	const transformer = transformerFactory.createTransformer();
+	const cacheKey = transformer.getCacheKey(undefined, undefined, {
+		instrument: value,
+	});
+	return cacheKey;
+});
+testInput('nodeEnv', function invoke(value) {
+	const transformerFactory = createTransformerFactory({ nodeEnv: value });
+	const transformer = transformerFactory.createTransformer();
+	const cacheKey = transformer.getCacheKey();
+	return cacheKey;
+});
+testInput('nodeVersion', function invoke(value) {
+	const transformerFactory = createTransformerFactory({ nodeVersion: value });
+	const transformer = transformerFactory.createTransformer();
+	const cacheKey = transformer.getCacheKey();
+	return cacheKey;
+});
 
 test.run();
