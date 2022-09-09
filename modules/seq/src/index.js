4a5aae9a6a7dd9ebc448d2bf44ab2554e05461c2
jzinn <jzinn>
2022-09-09 13:18:03 -0700

    construct

--- modules/seq/src/index.js
+++ modules/seq/src/index.js
@@ -1,10 +1,10 @@
 export default function seq(i = 1, step = 1) {
 	if (typeof i !== 'number') throw new TypeError(`not a number: ${i}`);
 	if (typeof step !== 'number') throw new TypeError(`not a number: ${step}`);
-	return SEQ(i, step);
+	return construct(i, step);
 }
 
-function* SEQ(i, step) {
+function* construct(i, step) {
 	while (true) {
 		yield i;
 		i += step;
--- modules/take/src/index.js
+++ modules/take/src/index.js
@@ -2,10 +2,10 @@ export default function take(n, values) {
 	if (typeof n !== 'number') throw new TypeError(`not a number: ${n}`);
 	if (typeof Object(values)[Symbol.iterator] !== 'function')
 		throw new TypeError(`not values: ${values}`);
-	return TAKE(n, values);
+	return construct(n, values);
 }
 
-function* TAKE(n, values) {
+function* construct(n, values) {
 	for (const value of values) {
 		if (n-- <= 0) break;
 		yield value;
