fc25f20b669d6956a5cbc6db16807bb71cc8c0c5
jzinn <jzinn>
2022-09-12 16:09:42 -0700

    nametag

--- modules/nametag/tests/index.js
+++ modules/nametag/tests/index.js
@@ -1,4 +1,18 @@
+import nametag from '#nametag';
+
 import { test } from 'uvu';
 import * as assert from 'uvu/assert';
+
+test('named object', function () {
+	assert.is(nametag`A${{ name: 'B' }}C`, 'ABC');
+});
+
+test('uses the value if the value has no name', function () {
+	assert.is(nametag`A${'B'}C`, 'ABC');
+});
+
+test('empty string named object', function () {
+	assert.is(nametag`A${{ name: '' }}C`, 'A[empty name]C');
+});
 
 test.run();
