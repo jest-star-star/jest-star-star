a8af59058aeaaebc1daafeaad93735eaf652294c
jzinn <jzinn>
2022-09-12 16:10:08 -0700

    propertytag

--- modules/propertytag/src/index.js
+++ modules/propertytag/src/index.js
@@ -4,11 +4,9 @@ export default function propertytag(property, [first, ...strings], ...values) {
 		return result + substitute(value) + strings[i];
 	}
 	function substitute(value) {
-		if (Object.hasOwn(value, property)) {
-			const result = `${value[property]}`;
-			if (result === '') return `[empty ${property}]`;
-			return result;
-		}
-		return `${value}`;
+		if (!Object.hasOwn(value, property)) return `${value}`;
+		const result = `${value[property]}`;
+		if (result === '') return `[empty ${property}]`;
+		return result;
 	}
 }
