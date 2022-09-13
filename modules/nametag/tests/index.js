ac04d2b64aa7df98bde63309a0c750fab3e54a16
jzinn <jzinn>
2022-09-13 14:00:59 -0700

    property tags

--- modules/nametag/tests/index.js
+++ modules/nametag/tests/index.js
@@ -7,12 +7,4 @@ test('named object', function () {
 	assert.is(nametag`A${{ name: 'B' }}C`, 'ABC');
 });
 
-test('uses the value if the value has no name', function () {
-	assert.is(nametag`A${'B'}C`, 'ABC');
-});
-
-test('empty string named object', function () {
-	assert.is(nametag`A${{ name: '' }}C`, 'A[empty name]C');
-});
-
 test.run();
--- modules/propertytag/tests/index.js
+++ modules/propertytag/tests/index.js
@@ -1,4 +1,22 @@
+import propertytag from '#propertytag';
+
 import { test } from 'uvu';
 import * as assert from 'uvu/assert';
+
+function PROPERTYtag(...args) {
+	return propertytag('PROPERTY', ...args);
+}
+
+test('object with property PROPERTY', function () {
+	assert.is(PROPERTYtag`A${{ PROPERTY: 'B' }}C`, 'ABC');
+});
+
+test('object with property PROPERTY that is an empty string', function () {
+	assert.is(PROPERTYtag`A${{ PROPERTY: '' }}C`, 'A[empty PROPERTY]C');
+});
+
+test('uses the value if the value has no property PROPERTY', function () {
+	assert.is(PROPERTYtag`A${'B'}C`, 'ABC');
+});
 
 test.run();
