70ac6dce52d8148e811f868ec23392d58b8b6a43
jzinn <jzinn>
2022-09-13 21:50:30 -0700

    object-shorthand

--- modules/reset/src/index.js
+++ modules/reset/src/index.js
@@ -1,5 +1,5 @@
 export default function reset(property, value, object) {
 	if (!Object.hasOwn(object, property))
 		throw new TypeError(`object must have property "${property}"`);
-	return Object.defineProperty(object, property, { value: value });
+	return Object.defineProperty(object, property, { value });
 }
