cb62f045edf87acda700747badc4c1fd58605915
jzinn <jzinn>
2022-09-18 19:23:55 -0700

    eslint: update config for airbnb

--- config.eslint.cjs
+++ config.eslint.cjs
@@ -1,20 +1,41 @@
+'use strict';
+
 module.exports = {
 	env: {
 		es6: true,
 		node: true,
 	},
-	extends: ['eslint:recommended', 'prettier'],
-	overrides: [],
+	extends: ['airbnb-base', 'prettier'],
+	overrides: [
+		{
+			files: ['**/*.cjs'],
+			parserOptions: {
+				sourceType: 'script',
+			},
+			rules: {
+				strict: ['error', 'global'],
+			},
+		},
+	],
 	parserOptions: {
 		ecmaVersion: 'latest',
 		sourceType: 'module',
 	},
-	rules: {},
 	root: true,
+	rules: {
+		'func-names': 'off',
+		'import/extensions': 'off',
+		'import/no-duplicates': 'off',
+		'import/no-extraneous-dependencies': 'off',
+		'import/no-unresolved': 'off',
+		'import/order': 'off',
+		'lines-between-class-members': 'off',
+		'no-param-reassign': 'off',
+		'no-plusplus': 'off',
+		'no-restricted-syntax': 'off',
+		'no-shadow': 'off',
+		'no-use-before-define': 'off',
+		'prefer-arrow-callback': 'off',
+		'sort-keys': ['error', 'asc', { caseSensitive: true, natural: true }],
+	},
 };
-
-// {
-//   "rules": {
-//     "no-tabs": ["error", {"allowIndentationTabs": true}]
-//   }
-// }
